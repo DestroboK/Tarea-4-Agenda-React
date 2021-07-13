b12086d7b3b1121144498a7515b345900972b8f2
Kelvin Hernandez <youmater2001@gmail.com>
2021-07-12 23:28:52 -0400

    Commit inicial. Proyecto creado y pruebas iniciales con la herramienta

---
+++ src/Saludador.js
@@ -0,0 +1,10 @@
+
+function Saludador(){
+    return(
+        <div className="saludador">
+            <p>Hola externo, usando un componente</p>
+        </div>
+    );
+}
+
+export default Saludador;
