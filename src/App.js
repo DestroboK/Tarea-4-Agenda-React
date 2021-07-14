4417059d8ebe924f20262b078608b35194882454
Kelvin Hernandez <youmater2001@gmail.com>
2021-07-13 21:33:06 -0400

    Agreagada la capacidad de mostrar todos los contactos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import Saludador from "./Saludador.js";
+import { useState } from 'react';
+import Agenda from './ContenedorPadre'
+
+function cargar(){
+
+  return fetch("http://www.raydelto.org/agenda.php")
+  .then(function(resultados){
+      return resultados.json();
+  }).then(function(resultados){
+    return resultados;
+  });
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <Saludador />
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p className="Titulo">Agenda de la sociedad secreta ITLA</p>
+            <Agenda/>
       </header>
     </div>
   );
---
+++ src/ContenedorPadre.js
@@ -0,0 +1,11 @@
+import CargarContactos from "./ListaContactos.js";
+function Agenda(){
+
+    return (
+        <CargarContactos/>
+    );
+    
+
+}
+
+export default Agenda
